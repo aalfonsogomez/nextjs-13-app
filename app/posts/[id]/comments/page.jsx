c4afdad9a1a994705563281b04640dcf02c85d7c
Angel <a.alfonso.gomez@gmail.com>
2022-11-25 12:14:16 +0100

    Probamos una pagina de loading en los comentarios

--- app/posts/[id]/comments/page.jsx
+++ app/posts/[id]/comments/page.jsx
@@ -1,5 +1,6 @@
 
-const fetchComments = (id) => {
+const fetchComments = async (id) => {
+  await new Promise(resolve => setTimeout(resolve, 5000))
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: {
       revalidate: 60
@@ -13,11 +14,11 @@ export default async function Comment ({ params }) {
   const comments = await fetchComments(id)
 
   return (
-    <ul>
+    <ul style={{ background: '#eee', fontSize: '10px' }}>
       {comments.map(comment => (
         <li key={comment.id}>
-          <h3>{comment.name}</h3>
-          <p>{comment.body}</p>
+          <h4>{comment.name}</h4>
+          <small>{comment.body}</small>
         </li>
       ))}
     </ul>
