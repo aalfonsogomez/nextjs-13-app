dec9762d8e53605ea1900e7e9cbb7e153fe46fdd
Angel <a.alfonso.gomez@gmail.com>
2022-11-28 12:37:37 +0100

    Mostramos mensaje error si no cargan los commits

---
+++ app/posts/[id]/comments/errors.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error () {
+  return (
+    <div>
+      <h1>Hubo un error</h1>
+      <p>Lo sentimos, hubo un error al cargar los comentarios. Por favor, intenta de nuevo.</p>
+    </div>
+  )
+}
