3bf3e39340189df2d3b545fcb9e42ae234c8cc74
Angel <a.alfonso.gomez@gmail.com>
2022-11-29 11:25:35 +0100

    Probamos @next/font

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
-
+import { Space_Grotesk } from '@next/font/google'
 import { Navigation } from './components/navigation'
+
+const font = Space_Grotesk({
+  weights: ['400', '700']
+})
 
 export default function RootLayout ({ children }) {
   return (
@@ -8,7 +12,7 @@ export default function RootLayout ({ children }) {
       <head>
         <title>My first App with Nextjs 13</title>
       </head>
-      <body>
+      <body className={font.className}>
         <Navigation />
         {children}
       </body>
